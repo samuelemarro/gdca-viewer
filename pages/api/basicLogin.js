1deae8aa67755d89a9edcbe1ff252419f974aa12
███████ <.@csp.unibo.it>
2022-10-10 23:26:55 +0200

    Fixed login.

--- pages/api/basicLogin.js
+++ pages/api/basicLogin.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-    if (req.query?.password == process.env.LOGIN_PASSWORD) {
+    if (req.query?.password.toLowerCase() == process.env.LOGIN_PASSWORD.toLowerCase()) {
         res.status(200).json(true)
     } else {
         res.status(401).json(false)
