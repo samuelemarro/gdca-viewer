3e241a185a414d7402f18a01b567ed2024aa5aaf
███████ <.@csp.unibo.it>
2022-10-11 23:41:18 +0200

    Added CdS.

---
+++ components/CdS.js
@@ -0,0 +1,22 @@
+export default function CdS () {
+    return (
+        <>
+            <p>Lettera informatica n. █████ della Divisione Amministrativa</p>
+            <p>
+                Attenzione: al fine di massimizzare l&apos;integrazione degli agenti CSP con il Collegio Superiore Irnerio, la Divisione Amministrativa
+                ha approvato un sistema di immunità qualificata all&apos;interno delle attività di Collegio. In particolare, è ora possibile ottenere
+                l&apos;immunità nell'attività collegiale "Collegio di Sangue" per le prime 12 ore dall&apos;inizio del gioco. Per far ciò, è necessario
+                contattare l&apos;organizzatore del Collegio di Sangue in modo da riferire la volontà di usufruire di questo bonus assicurandosi di usare
+                la parola chiave "MOAB".
+            </p>
+            <p>
+                L&apos;organizzatore del Collegio di Sangue non è a conoscenza della motivazione per cui alcuni collegiali abbiano diritto a questo
+                beneficio. Si ricorda quindi che rivelare la propria identità come membro del CSP comporta la terminazione immediata e l&apos;avvio di
+                un&apos;istanza ai sensi dell&apos;art. ███ del codice del CSP.
+            </p>
+            <p>
+                Si ricorda inoltre che il termine per fare questa richiesta coincide con l&apos;inizio del Collegio di Sangue.
+            </p>
+        </>
+    )
+}
