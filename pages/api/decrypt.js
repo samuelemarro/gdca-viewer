fdeb888271b1c889bab4af58ed3e5d226ead756d
███████ <.@csp.unibo.it>
2022-10-12 21:58:28 +0200

    c

--- pages/api/decrypt.js
+++ pages/api/decrypt.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
         key = key.toLowerCase().trim()
     }
     if (req.query?.tracking == 'BCIC') {
-        await answer({success: false})
+        await answer({success: false}, req, res)
         return
     }
     const matchingFile = getMatchingFile(key)
