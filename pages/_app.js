c265c806a678135b1529c053de65462723d55f93
███████ <.@csp.unibo.it>
2022-10-12 02:43:38 +0200

    Added warning message.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  const [messageWritten, setMessageWritten] = useState(false)
+
+  useEffect(() => {
+    if (!messageWritten) {
+      console.log('SI RICORDA CHE LA MANIPOLAZIONE NON AUTORIZZATA DEL DATABASE CSP È SOGGETTA ALL\'ART. ███ DEL CODICE DEL CSP.')
+      setMessageWritten(true)
+    }
+  }, [])
+
   return (
     <>
       <Head>
