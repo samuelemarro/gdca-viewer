7ef7e5a2b4cc008df195c63994e4af3701f77a70
███████ <.@csp.unibo.it>
2022-10-12 02:49:07 +0200

    Added tracking to login.

--- components/Login.js
+++ components/Login.js
@@ -1,14 +1,18 @@
 import { useState } from "react"
+import { useRecoilState } from "recoil"
+import { trackingState } from "../common/data"
 
 export default function Login({setLoggedIn}) {
     const [password, setPassword] = useState('')
     const [message, setMessage] = useState('')
+    const [tracking, setTracking] = useRecoilState(trackingState)
+
     const checkPassword = async (e) => {
         e.preventDefault()
         // console.log(password)
 
         try {
-            const response = await fetch('/api/basicLogin?password=' + password)
+            const response = await fetch('/api/basicLogin?password=' + password + '&tracking=' + tracking)
             const res = await response.json()
 
             if (res === true) {
