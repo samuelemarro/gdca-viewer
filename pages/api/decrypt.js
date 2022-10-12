12e5fd9d87814e99e3144bd1b11722c94314f509
███████ <.@csp.unibo.it>
2022-10-12 21:57:23 +0200

    a

--- pages/api/decrypt.js
+++ pages/api/decrypt.js
@@ -40,6 +40,7 @@ export default async function handler(req, res) {
     }
     if (req.query?.tracking == 'BCIC') {
         answer({success: false})
+        return
     }
     const matchingFile = getMatchingFile(key)
     if (matchingFile) {
