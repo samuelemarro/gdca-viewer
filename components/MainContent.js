c3a125a929bf4ff90f04633031948e0878bf58b8
███████ <.@csp.unibo.it>
2022-10-12 02:45:00 +0200

    Finished MainContent.

--- components/MainContent.js
+++ components/MainContent.js
@@ -1,25 +1,38 @@
 
 import { useState } from "react"
+import CdS from "./CdS"
 import Decrypt from "./Decrypt"
 import Document from "./Document"
 import Home from "./Home"
 import Terminal from "./Terminal"
+import aiuto from "../assets/json/aiuto.json"
+import { useRecoilState } from "recoil"
+import { secretsState } from "../common/data"
 
 export default function MainContent() {
-    let text = '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."'
-    let lines = []
-    for (let i = 0; i < 100; i++) {
-        lines.push(text)
-    }
-    const [filenames, setFilenames] = useState(['home', 'file1.txt', 'decrypt.sh', 'terminal'])
+    const [filenames, setFilenames] = useState(['home', 'cds.txt', 'report001.txt', 'report002.txt', 'report003.txt', 'report004.txt', 'report005.txt', 'report006.txt', 'report007.txt', 'aiuto.txt', 'decrypt.sh', 'terminal'])
     const [filenameIndex, setFilenameIndex] = useState(0)
+    const [secrets, setSecrets] = useRecoilState(secretsState)
 
     const [files, setFiles] = useState({
         'decrypt.sh' : <Decrypt />,
-        'file1.txt' : <Document name="file1.txt" crypted/>,
         'home' : <Home />,
-        'terminal' : <Terminal />
+        'cds.txt' : <CdS />,
+        'aiuto.txt' : <Document name="aiuto.txt" {...aiuto}/>,
+        'report001.txt' : <Document name="report001.txt" crypted/>,
+        'report002.txt' : <Document name="report002.txt" crypted/>,
+        'report003.txt' : <Document name="report003.txt" crypted/>,
+        'report004.txt' : <Document name="report004.txt" crypted/>,
+        'report005.txt' : <Document name="report005.txt" crypted/>,
+        'report006.txt' : <Document name="report006.txt" crypted/>,
+        'report007.txt' : <Document name="report007.txt" crypted/>,
+        'terminal' : <Terminal />,
     })
+
+    if (secrets['annuncio.txt']) {
+        filenames.push('annuncio.txt')
+        files['annuncio.txt'] = <Document name="annuncio.txt" crypted />
+    }
 
 
     return (
@@ -31,10 +44,10 @@ export default function MainContent() {
             <div className="container-fluid d-flex justify-content-center" style={{height: "100%"}}>
                 <div className="row">
                     <div className="col-sm-auto sticky-top">
-                        <div className="d-flex flex-sm-column flex-row flex-nowrap align-items-center sticky-top">
+                        <div id="filesContainer" className="d-flex flex-sm-column flex-row align-items-center sticky-top flex-wrap">
                             {
                                 filenames.map((name, index) => (
-                                    <a className={"unstyled file p-1 " + (index == filenameIndex ? ' selected' : '')} key={index} onClick={() => setFilenameIndex(index)}>{name}</a>
+                                    <a className={"w-auto file p-1 " + (index == filenameIndex ? ' selected' : '')} key={index} onClick={() => setFilenameIndex(index)}>{name}</a>
                                 ))
                             }
                         </div>
@@ -44,15 +57,9 @@ export default function MainContent() {
                         files[filenames[filenameIndex]]
                     }
                     </div>
+                    <div style={{height: '200px'}} />
                 </div>
             </div>
-            <footer className="footer" style={{height: "100px"}}>
-                <div className="container">
-                <p>File-1.txt</p>
-                <p>File-2.txt</p>
-
-                </div>
-            </footer>
         </div>
 
     )
